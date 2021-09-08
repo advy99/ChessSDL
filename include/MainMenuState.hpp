0cd2095793d0321e830f3f18d5e5697b8ab4404d
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-08 22:46:51 +0200

    MainMenuState: create main menu

---
+++ include/MainMenuState.hpp
@@ -0,0 +1,31 @@
+#ifndef MAINMENUSTATE_H_INCLUDED
+#define MAINMENUSTATE_H_INCLUDED
+
+#include "MenuState.hpp"
+#include "GameObject.hpp"
+#include <vector>
+
+class MainMenuState : public MenuState {
+	private:
+		static const std::string menu_id;
+
+
+		static void menu_to_play();
+		static void exit_from_menu();
+
+
+		virtual void set_callbacks(const std::vector<Callback> & callbacks);
+
+	public:
+		virtual ~MainMenuState();
+		virtual void update();
+		virtual void render();
+
+		virtual bool on_enter();
+
+		virtual std::string get_state_id() const;
+
+};
+
+
+#endif
---
+++ src/MainMenuState.cpp
@@ -0,0 +1,76 @@
+#include "MainMenuState.hpp"
+#include "InputHandler.hpp"
+#include "Game.hpp"
+#include "MenuButton.hpp"
+#include <iostream>
+#include "StateParser.hpp"
+
+MainMenuState::~MainMenuState() {
+
+}
+
+void MainMenuState :: update(){
+	if (!GameStateMachine::is_changing()) {
+		for(auto & object: objects_) {
+			object.update();
+		}
+	}
+}
+
+void MainMenuState :: render() {
+	if (!GameStateMachine::is_changing()) {
+		for(auto & object: objects_) {
+			object.draw();
+		}
+	}
+
+}
+
+
+bool MainMenuState :: on_enter() {
+	bool success = true;
+
+	StateParser parser;
+
+	parser.parseState("assets/test.xml", menu_id, &objects, &texture_id_list);
+
+	callbacks.push_back(menu_to_play);
+	callbacks.push_back(exit_from_menu);
+
+	set_callbacks(callbacks);
+
+	return success;
+
+}
+
+
+std::string MainMenuState::get_state_id() const {
+	return menu_id;
+}
+
+
+void MainMenuState::menuToPlay() {
+	std::cout << "Clicked Play button" << std::endl;
+
+	Game::change_state(PlayState());
+}
+
+
+void MainMenuState::exit_from_menu() {
+	std::cout << "Clicked Exit button" << std::endl;
+	Game::stop_running();
+}
+
+
+void MainMenuState::set_callbacks(const std::vector<Callback> & callbacks) {
+	for ( unsigned i = 0; i < objects.size(); i++ ) {
+		MenuButton * button = dynamic_cast<MenuButton*> (objects[i]) ;
+		if ( button != nullptr ) {
+			button->setCallback(callbacks[button->getCallbackID() ]);
+		}
+	}
+}
+
+
+
+const std::string MainMenuState::menu_id = "MENU";
