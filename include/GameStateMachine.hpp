df7a20c99d9be7b2dfb4b4288e1766baf094e6fd
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-08 19:50:38 +0200

    GameStateMachine: create class

---
+++ include/GameStateMachine.hpp
@@ -0,0 +1,29 @@
+#ifndef GAMESTATEMACHINE_H_INCLUDED
+#define GAMESTATEMACHINE_H_INCLUDED
+
+#include "GameState.h"
+#include <stack>
+
+class GameStateMachine {
+	private:
+		std::stack<GameState> game_states_;
+		static bool changing_;
+
+
+	public:
+		~GameStateMachine() = default;
+		void push(const GameState & state);
+		void change(const GameState & state);
+		void pop();
+
+		void update();
+		void render();
+
+		static bool is_changing() ;
+
+
+};
+
+
+
+#endif
---
+++ src/GameStateMachine.cpp
@@ -0,0 +1,51 @@
+#include "GameStateMachine.hpp"
+
+void GameStateMachine :: push(const GameState & state) {
+	game_states_.push(state);
+	game_states_.top().on_enter();
+
+	changing_ = true;
+}
+
+void GameStateMachine :: change(const GameState & state) {
+
+	if ( !game_states_.empty() ) {
+		if ( game_states_.top().get_state_id() != state_->get_state_id() ){
+			pop();
+			push(state);
+		}
+	} else {
+		push(state);
+	}
+
+	changing = true;
+
+}
+
+
+void GameStateMachine :: pop() {
+	if ( !game_states_.empty() ) {
+		changing_ = true;
+		game_states_.pop();
+	}
+}
+
+void GameStateMachine :: update() {
+	changing_ = false;
+	if ( !game_states_.empty() ) {
+		game_states.top().update();
+	}
+}
+
+void GameStateMachine :: render() {
+	if ( !game_states.empty() ) {
+		game_states.top().render();
+	}
+}
+
+bool GameStateMachine :: isChanging() {
+	return changing;
+}
+
+
+bool GameStateMachine :: changing = false;
