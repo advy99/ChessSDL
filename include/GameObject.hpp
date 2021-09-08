b7e4fa73cb87d9a47a766f325f3989883db70a53
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-08 23:13:52 +0200

    GameObject interface

---
+++ include/GameObject.hpp
@@ -0,0 +1,29 @@
+#ifndef GAMEOBJECT_H_INCLUDED
+#define GAMEOBJECT_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "TextureManager.hpp"
+#include "LoaderParams.hpp"
+#include <string>
+
+class GameObject {
+	private:
+
+	protected:
+		GameObject() {};
+
+
+	public:
+		virtual void draw() = 0;
+		virtual void update() = 0;
+		virtual void clean() = 0;
+		virtual void load(const LoaderParams * params) = 0;
+
+		virtual ~GameObject(){};
+};
+
+
+
+
+#endif
