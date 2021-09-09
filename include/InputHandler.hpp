177db336108fc244d4af2a633afa81ec252a24db
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-09-09 09:11:04 +0200

    InputHandler: start working in a static class

--- include/InputHandler.hpp
+++ include/InputHandler.hpp
@@ -5,7 +5,7 @@
 #include "Vector2D.hpp"
 #include <SDL2/SDL.h>
 
-enum mouse_buttons {
+enum class mouse_buttons {
 	LEFT = 0,
 	MIDDLE = 1,
 	RIGHT = 2
@@ -13,46 +13,44 @@ enum mouse_buttons {
 
 class InputHandler {
 	private:
-		bool joysticks_initialised_;
-		std::vector<SDL_Joystick *> joysticks_;
-		std::vector<std::pair<Vector2D , Vector2D > > joystick_values_;
-		std::vector<std::vector<bool> > button_states_;
+		static bool joysticks_initialised_;
+		static std::vector<SDL_Joystick *> joysticks_;
+		static std::vector<std::pair<Vector2D , Vector2D > > joystick_values_;
+		static std::vector<std::vector<bool> > button_states_;
 
-		std::vector<bool> mouse_button_states_;
+		static std::vector<bool> mouse_button_states_;
 
-		Vector2D mouse_position_;
-		Uint8 * key_state_;
+		static Vector2D mouse_position_;
+		static Uint8 * key_state_;
 
-		void on_joystick_axis_move(const SDL_Event event);
-		void on_joystick_button_down(const SDL_Event event);
-		void on_joystick_button_up( const SDL_Event event );
-		void on_mouse_button_down( const SDL_Event event );
-		void on_mouse_button_up( const SDL_Event event );
-		void on_mouse_motion ( const SDL_Event event );
-
-
+		static void on_joystick_axis_move(const SDL_Event event);
+		static void on_joystick_button_down(const SDL_Event event);
+		static void on_joystick_button_up( const SDL_Event event );
+		static void on_mouse_button_down( const SDL_Event event );
+		static void on_mouse_button_up( const SDL_Event event );
+		static void on_mouse_motion ( const SDL_Event event );
 
 
-		const int joystick_dead_zone_ = 10000;
+
+
+		static const int joystick_dead_zone_ = 10000;
 
 	public:
-		InputHandler();
-		~InputHandler();
 
-		void update();
+		static void update();
 
-		void initialise_joysticks();
-		bool joysticks_initialised() const;
+		static void initialise_joysticks();
+		static bool joysticks_initialised() const;
 
-		int x_value(const int joy, const int stick) const;
-		int y_value(const int joy, const int stick) const;
-		bool get_button_state(const int joy, const int button_number) const;
-		bool get_mouse_button_state(const int button) const;
+		static int x_value(const int joy, const int stick) const;
+		static int y_value(const int joy, const int stick) const;
+		static bool get_button_state(const int joy, const int button_number) const;
+		static bool get_mouse_button_state(const int button) const;
 
-		Vector2D get_mouse_position() const;
-		bool is_key_down(const SDL_Scancode key) const;
+		static Vector2D get_mouse_position() const;
+		static bool is_key_down(const SDL_Scancode key) const;
 
-		void reset();
+		static void reset();
 
 };
 
