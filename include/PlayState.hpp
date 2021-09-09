96f3f984887c49590fa79d93e31cb80a1d9d9f72
advy99 <advy99@gmail.com>
2021-09-09 15:35:32 +0200

    PlayState: new state for the play state

---
+++ include/PlayState.hpp
@@ -0,0 +1,30 @@
+#ifndef PLAYSTATE_H_INCLUDED
+#define PLAYSTATE_H_INCLUDED
+
+#include "GameState.hpp"
+#include "GameObject.hpp"
+#include "SDLGameObject.hpp"
+#include <vector>
+#include "Level.hpp"
+
+class PlayState : public GameState {
+	private:
+		static const std::string play_id_;
+		bool check_collision(const SDLGameObject & p1, const SDLGameObject & p2);
+
+		std::unique_ptr<Level> level_;
+
+	public:
+		virtual ~PlayState();
+		virtual void update();
+		virtual void render();
+
+		virtual bool on_enter();
+
+		virtual std::string get_state_id() const;
+
+};
+
+
+
+#endif
---
+++ src/PlayState.cpp
@@ -0,0 +1,64 @@
+#include "PlayState.hpp"
+#include "InputHandler.hpp"
+#include "PauseState.hpp"
+#include "GameOverState.hpp"
+#include "Game.hpp"
+#include <iostream>
+#include "StateParser.hpp"
+#include "LevelParser.hpp"
+
+
+
+PlayState::~PlayState() {
+}
+
+void PlayState::update() {
+	level_.update();
+
+}
+
+void PlayState::render() {
+	level_.render();
+}
+
+bool PlayState :: on_enter() {
+	std::cout << "Entering Play state" << std::endl;
+
+	bool success = true;
+
+	level_ = std::make_unique(LevelParser::parse_level("assets/first_csv.tmx"));
+
+	return success;
+}
+
+std::string PlayState :: get_state_id() const {
+	return play_id_;
+}
+
+bool PlayState::check_collision(const SDLGameObject & p1,
+										  const SDLGameObject & p2){
+	bool collision = false;
+
+
+	int leftA = p1.get_position().get_x();
+	int rightA = leftA + p1.get_width();
+	int topA = p1.get_position().get_y();
+	int bottomA = topA + p1.get_height();
+
+
+	int leftB = p2.get_position().get_x();
+	int rightB = leftB + p2.get_width();
+	int topB = p2.get_position().get_y();
+	int bottomB = topB + p2.get_height();
+
+
+	collision = ( bottomA >= topB && topA <= bottomB ) &&
+					( rightA >= leftB && leftA <= rightB );
+
+
+	return collision;
+}
+
+
+
+const std::string PlayState::play_id_ = "PLAY";
