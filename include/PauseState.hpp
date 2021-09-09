d008c223572fd6d2b15009613d0f2a5afec61a2b
advy99 <advy99@gmail.com>
2021-09-09 15:35:52 +0200

    PauseState: new state

---
+++ include/PauseState.hpp
@@ -0,0 +1,32 @@
+#ifndef PAUSESTATE_H_INCLUDED
+#define PAUSESTATE_H_INCLUDED
+
+#include "MenuState.hpp"
+#include "GameObject.hpp"
+#include <vector>
+
+class PauseState : public MenuState {
+	private:
+		static void pause_to_main();
+		static void resume_play();
+
+		static const std::string pause_id_;
+
+
+		virtual void set_callbacks(const std::vector<Callback> & callbacks);
+
+	public:
+		virtual ~PauseState();
+
+		virtual void update();
+		virtual void render();
+
+		virtual bool on_enter();
+
+		virtual std::string get_state_id() const;
+
+
+};
+
+
+#endif
---
+++ src/PauseState.cpp
@@ -0,0 +1,65 @@
+#include "PauseState.hpp"
+#include "Game.hpp"
+#include "MainMenuState.hpp"
+#include "MenuButton.hpp"
+#include "InputHandler.hpp"
+#include "StateParser.hpp"
+
+PauseState::~PauseState() {
+	
+}
+
+std::string PauseState::get_state_id() const {
+	return pause_id_;
+}
+
+void PauseState::pause_to_main(){
+	Game::change_state(MainMenuState());
+}
+
+
+void PauseState::resume_play() {
+	Game::pop_state();
+}
+
+void PauseState::update() {
+	for ( unsigned i = 0; i < objects_.size() &&
+								!GameStateMachine::is_changing(); i++ ) {
+		objects_[i]->update();
+	}
+}
+
+
+void PauseState::render() {
+	for ( unsigned i = 0; i < objects_.size() &&
+								!GameStateMachine::is_changing(); i++ ) {
+		objects_[i]->draw();
+	}
+}
+
+bool PauseState::on_enter() {
+	bool success = true;
+
+
+	StateParser::parse_state("assets/test.xml", pause_id_, objects_);
+
+	callbacks_.push_back(pause_to_main);
+	callbacks_.push_back(resume_play);
+
+	set_callbacks(callbacks_);
+
+	return success;
+}
+
+
+void PauseState::set_callbacks(const std::vector<Callback> & callbacks) {
+	for ( unsigned i = 0; i < objects_.size(); i++ ) {
+		MenuButton * button = dynamic_cast<MenuButton*> (&objects_[i]) ;
+		if ( button != nullptr ) {
+			button->set_callback(callbacks[button->get_callback_id() ]);
+		}
+	}
+}
+
+
+const std::string PauseState::pause_id_ = "PAUSE";
